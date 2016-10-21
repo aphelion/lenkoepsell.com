8c4b26d667ad2c035c375b4b7cd3845027695809
Leonhardt Koepsell <leonhardt@koepsell.io>
2016-10-20 22:43:21 -0500

    Iterate on design 😆

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     $('[data-mobile-menu-button]').click(function () {
         $('[data-mobile-menu]').toggle();
+        $('.mobile-menu').toggleClass('open');
+    });
+
+    $('[data-desktop-menu-button]').click(function () {
+        $('[data-desktop-menu]').toggle();
     });
 });
